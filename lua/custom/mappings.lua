ba76ca954acaa81e40463056d85dcfc145c0f11f
Brady Bodily <brady.bodily@sdl.usu.edu>
2024-04-03 19:31:12 -0600

    working lua configs

---
+++ lua/custom/mappings.lua
@@ -0,0 +1,30 @@
+local M = {}
+
+M.dap = {
+  plugin = true,
+  n = {
+    ["<leader>db"] = {
+      "<cmd> DapToggleBreakpoint <CR>",
+      "Add breakpoint at line",
+    },
+    ["<leader>dr"] = {
+      "<cmd> DapContinue <CR>",
+      "Start or continue the debugger",
+    },
+    ["<leader>cb"] = {
+      "<cmd> CMakeBuild <CR>",
+      "Run CMakeBuild",
+    },
+    ["<leader>cr"] = {
+      "<cmd> CMakeRun <CR>",
+      "Run CMake",
+    },
+    ["<leader>cd"] = {
+      "<cmd> CMakeDebug <CR>",
+      "Run CMakeDebug",
+    }
+    
+  }
+}
+
+return M
